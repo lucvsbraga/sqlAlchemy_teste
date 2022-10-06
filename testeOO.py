b1567a6ad9e596fe148372c885ec932f1a8b3674
Lucas Braga <lucas.braga@msn.com>
2022-10-05 22:46:20 -0300

    Testando SQLAlchemy

--- testeOO.py
+++ testeOO.py
@@ -2,15 +2,6 @@ from session import Banco
 from student import Student
 
 banco = Banco()
-# engine = create_engine(
-#     "mysql+pymysql://root:Senha2mysql.@localhost:3306/bigbox", echo=False)
-
-# Session = sessionmaker(bind=engine)
-# session = Session()
-
-student3 = Student(name='Tales', age=30, grade='Fifth')
-# session.add(student3)
-# session.commit()
 
 student = banco.session.query(Student).filter(
     Student.name == 'Lucas Braga').first()
